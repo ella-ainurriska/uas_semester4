edccf9cbfe8c8aacbb40b6bf9bfb24b746c70507
icha <ellaainurriska1@gmail.com>
2025-06-19 21:52:39 +0700

    Integrasi API jadwal sholat

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../services/api_sholat_service.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +10,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+Map<String, dynamic>? jadwal;
+bool isLoading = true;
+bool isError = false;
+
+@override
+void initState() {
+  super.initState();
+  getJadwal();
+}
+
+Future<void> getJadwal() async {
+  try {
+    final data = await SholatService.fetchJadwal();
+    setState(() {
+      jadwal = data;
+      isLoading = false;
+      isError = false;
+    });
+  } catch (e) {
+    setState(() {
+      isLoading = false;
+      isError = true;
+    });
+  }
+}
+
+
   int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
@@ -16,13 +45,23 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+ 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: isLoading
+        ? Center(child: CircularProgressIndicator())
+        : isError
+        ? Center(child: Text('Gagal memuat jadwal sholat.'))
+        : Center(
+            child: Text("Subuh: ${jadwal!['jadwal']['subuh']}"),
+          ),
+
       appBar: AppBar(
         title: Text('Aplikasi Sholat'),
         backgroundColor: Color(0xFF1ABC9C),
-      ),
+      )
       body: Center(child: Text('Home Screen')),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
