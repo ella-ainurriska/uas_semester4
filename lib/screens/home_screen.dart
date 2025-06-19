542e1624126a516dd759bea95f0fcda88cf316be
icha <ellaainurriska1@gmail.com>
2025-06-19 21:39:13 +0700

    Struktur awal halaman HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Beranda'),
+        title: Text('Aplikasi Sholat'),
+        backgroundColor: Color(0xFF1ABC9C),
       ),
-      body: Center(
-        child: Text('Selamat datang di Ngaji Lite!'),
+      body: Center(child: Text('Home Screen')),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Color(0xFF1ABC9C),
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          
+        ],
       ),
     );
   }
