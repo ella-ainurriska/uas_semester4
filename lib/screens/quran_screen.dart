ed7e5177e1bd8e5194c6e84ebea707d891f75524
icha <ellaainurriska1@gmail.com>
2025-06-20 20:24:15 +0700

    Tampilkan TabBar dari daftar surah

--- lib/screens/quran_screen.dart
+++ lib/screens/quran_screen.dart
@@ -1,4 +1,7 @@
+// quran_screen.dart
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:coba3/models/surah_model.dart';
 
 class QuranScreen extends StatefulWidget {
@@ -14,14 +17,61 @@ class _QuranScreenState extends State<QuranScreen> {
   bool isError = false;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  void initState() {
+    super.initState();
+    fetchSurah();
+  }
+
+  Future<void> fetchSurah() async {
+    final url = Uri.parse('https://api.myquran.com/v2/quran/surat/semua');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final List<dynamic> data = jsonData['data'];
+        final List<Surah> surahList =
+            data.map((json) => Surah.fromJson(json)).toList();
+
+        setState(() {
+          _surahList = surahList;
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Gagal fetch data: ${response.body}');
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+        isError = true;
+      });
+    }
+  }
+
+ @override
+Widget build(BuildContext context) {
+  return DefaultTabController(
+    length: _surahList.length,
+    child: Scaffold(
       appBar: AppBar(
         title: Text("Al-Qur'an"),
+        bottom: isLoading || isError
+            ? null
+            : TabBar(
+                isScrollable: true,
+                tabs: _surahList
+                    .map((surah) => Tab(text: surah.namaLatin))
+                    .toList(),
+              ),
       ),
-      body: Center(
-        child: Text("Menyiapkan data..."),
-      ),
-    );
-  }
-}
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : isError
+              ? Center(child: Text("Gagal memuat data."))
+              : TabBarView(
+                  children:
+                      _surahList.map((_) => Center(child: Text("..."))).toList(),
+                 ),
+              ),
+            );
+          }
+        }
