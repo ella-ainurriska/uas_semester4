0606dadb208d8d3d822ba947f894dfc96b9eaf1c
icha <ellaainurriska1@gmail.com>
2025-06-21 09:39:33 +0700

    menmbahkan API alquran

--- lib/services/api_sholat_service.dart
+++ lib/services/api_sholat_service.dart
@@ -1,8 +1,20 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
+import 'package:coba3/models/ayat_model.dart';
 
 class SholatService {
+  static Future<List<Ayat>> fetchAyat(int nomorSurah) async {
+    final url = 'https://equran.id/api/surat/$nomorSurah';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      final List ayatJson = jsonData['ayat'];
+      return ayatJson.map((item) => Ayat.fromJson(item)).toList();
+    } else {
+      throw Exception('Gagal memuat ayat');
+    }
+  }
   static Future<Map<String, dynamic>> fetchJadwal() async {
     final now = DateTime.now();
     final String tanggal = "${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}";
