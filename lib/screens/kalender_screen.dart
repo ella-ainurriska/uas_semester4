9030e95bd8d2859e0385aec9ceac65f17d6a27f8
icha <ellaainurriska1@gmail.com>
2025-06-21 11:36:51 +0700

    menambahkan file kalender

---
+++ lib/screens/kalender_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KalenderScreen extends StatelessWidget {
+  const KalenderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
